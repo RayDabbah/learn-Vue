70b2b82f07a6c813b91b4e1a0bf29c9d5bd6d849
Rachamim Dabbah <dabbahray@yahoo.com>
2017-09-12 13:09:30 -0400

    initial commit

---
+++ newVue.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!!'
+    }
+})
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: `You started learning Vue on ${new Date().toLocaleString()}`
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
