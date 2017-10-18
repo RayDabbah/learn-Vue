4291fdd5b3550010194aa83678b0de76c98298bc
raydabbah <dabbahray@yahoo.com>
2017-10-18 15:06:25 -0400

    started new exercise

---
+++ watchComputedExercise.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+        books:[
+            {name: 'Cookbook for you', date: '1969'},
+            {name: 'The Mystery of mysteries', date: '1999'},
+            {name: 'How to build a shed', date: '2011'},
+            {name: 'American Roadtrips', date: '2000'},
+        ]
+
+    }
+})
+
+ 
+/* var app = new Vue({
+    el:'#app',
+    data:{
+        
+    }
+}) */
