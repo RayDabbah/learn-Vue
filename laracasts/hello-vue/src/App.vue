e61cd2c08a1bee0b69da22ace86bc805a3250ae8
raydabbah <dabbahray@yahoo.com>
2018-02-21 11:55:28 -0500

    first .vue file

---
+++ laracasts/hello-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+   <message>This is an amazing message!</message>
+   <message>This is a super amazing message!</message>
+   <message>This is a super duper amazing message!</message>
+  </div>
+</template>
+
+<script>
+import Message from './Message.vue';
+export default {
+    name: 'app',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App',
+        };
+    },
+    components: { Message},
+};
+</script>
+
+<style>
+
+</style>
---
+++ laracasts/hello-vue/src/Message.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="message">
+      <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+.message{
+    background: gray;
+    border-radius: .25rem;
+    border: green 3px solid;
+    margin: 2rem;
+    height: 3rem;
+    padding: 2rem;
+    text-align: center;
+    font-size: 1.5rem;
+}
+</style>
+
+
